d008dc63cbf4a202dd1c128e93012c20f56094e5
unknown <chaitu94dev@gmail.com>
2021-07-09 17:31:45 +0530

    moved project to master

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+  
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@RequestMapping("/get/{name}")
+	public String showData(@PathVariable("name") String name)
+	{
+		return "Hi "+name;
+	}
+}
